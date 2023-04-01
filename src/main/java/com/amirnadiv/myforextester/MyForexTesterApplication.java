29bfc34d383c28f5c37634af388f5506347673cf
amirnadiv <amirnadiv@gmail.com>
2023-04-01 08:16:15 +0300

    Initial commit

---
+++ src/main/java/com/amirnadiv/myforextester/MyForexTesterApplication.java
@@ -0,0 +1,13 @@
+package com.amirnadiv.myforextester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyForexTesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyForexTesterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amirnadiv/myforextester/MyForexTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amirnadiv.myforextester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyForexTesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
